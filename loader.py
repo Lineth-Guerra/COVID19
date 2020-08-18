b86a8e048b3f17b9ecc19bf396705ff83106a874
Lineth-Guerra <69860662+Lineth-Guerra@users.noreply.github.com>
2020-08-18 10:31:22 -0500

    Add files via upload

--- loader.py
+++ loader.py
@@ -6,7 +6,7 @@ def cargarModelo():
     FILENAME_MODEL_TO_LOAD = "covid19_model_full.h5"
     MODEL_PATH = "../../model"
 
-    # Cargar la RNA desde disco
+    #cargando el modelo de IA
     loaded_model = load_model(MODEL_PATH + "/" + FILENAME_MODEL_TO_LOAD)
     print("Modelo cargado de disco << ", loaded_model)
 
